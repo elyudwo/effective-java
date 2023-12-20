2c13765de547ba13cc90d12e9098b703d3c92ee0
elyudwo <dudqk9696@naver.com>
2023-12-20 19:35:10 +0900

    4 : [4 인스턴스화를 막으려거든 private_생성자를 사용하라]

---
+++ src/main/java/org/items/item4/Util.java
@@ -0,0 +1,11 @@
+package org.items.item4;
+
+public class Util {
+    private Util() {
+        throw new RuntimeException();
+    }
+
+    public static int plus(int a, int b) {
+        return a + b;
+    }
+}
